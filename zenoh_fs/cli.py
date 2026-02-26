86da39216778579af2adb0c4cec4a43a72c71ac4
Alex Davies <traverse.da@gmail.com>
2026-02-25 21:03:28 -0400

    feat: add ticket-based security and extended file operations

--- zenoh_fs/cli.py
+++ zenoh_fs/cli.py
@@ -1,13 +1,36 @@
+"""
+Zenoh FS exposes an arbitray universal pathlib compatible filesystem (local, s3, sshfs, ftp, etc) over zenoh.
+
+We split the filesystem into two sub trees, read and write. This makes us compatible with the existing ACL model of zenoh,
+allowing you to set ACLs like "hostname/files/write/var/log/myservice/**" to allow write access to a specific directory, of
+allowing read access to a specific file with "hostname/files/read/var/log/myservice/myfile.log". Like all of zenoh this supports
+wildcards in paths, allowing simple centralized policy-based access control to files and directories.
+
+On the client side, we implement an fsspect compatible filesystem object backed by zenoh, which we leverage to implement a FUSE filesystem. 
+This also allow you to use univeral pathlib to interact with our exposed filesystem as if it was a python pathlib object.
+
+Read and write support "methods" as parameters. This is always based on our READ and WRITE paths. A hash operation doesn't modify data, so
+it's a method on the read path, while delete is a method on the write path. This allows us to support operations other than just reading and writing bytes, while still being compatible with the existing ACL model of zenoh.
+
+We also have a capability-based security system using "tickets" that allow you to pass your permission to read/write a file to another client, bypassing the ACLs for that specific file.
+This is specifically useful for copy operations, where you can tell the fileserver itself to copy a file from source to destination, without having to read the file into memory on the client side before writing it back to zenoh.
+This is much more bandwidth efficient.
+"""
+
 from loguru import logger
 import typer
 import socket
 import zenoh
 from upath import UPath
 import time
-import urllib
 from fsspec.spec import AbstractFileSystem
 from typing import Generator, Tuple, Callable
 import hashlib
+import secrets
+import time
+import heapq
+import os
+import threading
 
 hostname = socket.gethostname()
 logger.info(f"Running on {hostname}")
@@ -18,149 +41,242 @@ app = typer.Typer()
 exposed_fs = None
 
 class FileServer:
-    def handler_read(self, query):
+
+    def normalize_path(self, path):
+        """Returns just the file path relative to the root of the exposed filesystem, removing the zenoh path and any read/write/ticket prefixes."""
+        #Remove our node location
+        path=path.removeprefix(self.zenoh_path)
+        #Remove ticket if it exists
+        path=path.removeprefix("/ticket")
+        #Remove read/write if it exists. We only remove one, so if someone does read/write.txt they read the file at the path /write.txt
+        if path.startswith("/read"):
+            path=path.removeprefix("/read")
+        elif path.startswith("/write"):
+            path=path.removeprefix("/write")
+        return path
+
+
+    def generate_ticket(self, file_path, method):
+        token = secrets.token_hex()
+        ticket_path = f"{self.zenoh_path}/ticket/{method}/{file_path}"
+        ticket = ticket_path + f"?token={token}"
+
+        with self.ticket_lock:
+            self.tickets[ticket] = time.time()
+            expiry_time = time.time() + self.ticket_ttl
+            heapq.heappush(self.ticket_heap, (expiry_time, ticket))
+            logger.debug(f"Generated ticket for `{ticket_path}` with token {token[:4]}**** and expiry time {expiry_time}")
+        return ticket
+
+    def handle_read(self, query):
+        method = query.parameters.get("method", "read")
         start = query.parameters.get("start", 0)
         end = query.parameters.get("end", None)
-        chunk_size = query.parameters.get("chunk_size", 1024*1024)
-        #ToDo: must set a max chunk size to avoid memory issues.
+        chunk_size = query.parameters.get("chunk_size", 1024*1024) 
         file_path = self.normalize_path(query.key_expr)
-        logger.info(f"Reading file at {file_path} with parameters: start={start}, end={end}, chunk_size={chunk_size}")
-        
-        with self.exposed_fs.open(file_path, "rb") as f:
-            f.seek(start)
-            for _ in range(0, end-start, chunk_size):
-                chunk = f.read(chunk_size)
-                if not chunk:
-                    break
-                query.reply(chunk)
 
-    def handler_list(self, query):
-        dir_path = self.normalize_path(query.key_expr)
-        logger.info(f"Listing directory at {dir_path}")
-        if self.exposed_fs.is_dir(dir_path):
-            for entry in self.exposed_fs.iterdir(dir_path):
-                query.reply(str(entry.name))
-        else:
-            query.reply_err(f"Error: {dir_path} is not a directory")
-
-    def handler_write(self, query):
-        file_path = self.normalize_path(query.key_expr)
-        logger.info(f"Writing to file at {file_path}")
-        with self.exposed_fs.open(file_path, "wb") as f:
-            for chunk in query.payload:
-                f.write(chunk)
-
-    def deterministic_walk(self, path):
-        """Walk the directory tree in a deterministic way by sorting the entries at each level."""
-        for entry in sorted(path.iterdir()):
-            yield entry
-            if entry.is_dir():
-                yield from self.deterministic_walk(entry)
-
-
-    def handler_hash(self, query):
-        """Generate a hash for a file or directory using a hash of hashes."""
-        #ToDo: Not sure about this one. It breaks our ACL a bit, since it can access files
-        #outside of the users tree.
-        hash_algo = query.parameters.get("algo", "sha256")
-        manifest = query.parameters.get("manifest", False)
-        file_path = self.normalize_path(query.key_expr)
-        logger.info(f"Hashing file at {file_path} with algorithm {hash_algo}")
-
-        # Validate algorithm
-        supported_algos = hashlib.algorithms_available
-        if hash_algo not in supported_algos:
-            query.reply_err(f"Error: Unsupported hash algorithm {hash_algo}. Supported algorithms are: {', '.join(supported_algos)}")
+        if method == "ticket":
+            query.reply(self.generate_ticket(file_path, "read").encode())
             return
-
-        # Initialize the root hash object
-        root_hash = hashlib.new(hash_algo)
-
-        if self.exposed_fs.is_dir(file_path):
-            # Process directory using deterministic_walk for sorted order
-            for entry in self.deterministic_walk(self.exposed_fs / file_path):
-
-                # Initialize a hash for this specific entry (file or folder)
-                entry_hash = hashlib.new(hash_algo)
-
-                # Hash the entry name (this ensures empty folders and filenames affect the root hash)
-                entry_hash.update(entry.name.encode('utf-8'))
-
-                if entry.is_file():
-                    # Hash the file content
-                    with entry.open("rb") as f:
-                        while True:
-                            chunk = f.read(1024*1024) # 1MB chunks
-                            if not chunk:
-                                break
-                            entry_hash.update(chunk)
-
-                # Update the root hash with the digest of the entry (The "Hash of Hashes" step)
-                # This is efficient because we only pass the small digest (e.g., 32 bytes) 
-                # to the parent hash, not the entire file content again.
-                root_hash.update(entry_hash.digest())
-
-                # Output manifest if requested
-                if manifest:
-                    query.reply(f"{entry}:{hash_algo}:{entry_hash.hexdigest()}")
-
-        else:
-            # Handle single file input
-            entry_hash = hashlib.new(hash_algo)
+   
+        if method == "hash":
+            logger.info(f"Hashing file at {file_path}")
+            hash_md5 = hashlib.md5()
             with self.exposed_fs.open(file_path, "rb") as f:
-                while True:
-                    chunk = f.read(1024*1024)
+                for chunk in iter(lambda: f.read(4096), b""):
+                    hash_md5.update(chunk)
+            query.reply(hash_md5.hexdigest().encode())
+            return
+        
+        if method == "size":
+            file_path = self.normalize_path(query.key_expr)
+            logger.info(f"Getting size of file at {file_path}")
+            size = self.exposed_fs.stat(file_path).st_size
+            query.reply(str(size).encode())
+            return
+        
+        if method == "list":
+            dir_path = self.normalize_path(query.key_expr)
+            logger.info(f"Listing directory at {dir_path}")
+            entries = [entry.name for entry in self.exposed_fs.iterdir(dir_path)]
+            query.reply("\n".join(entries).encode())
+            return
+        
+        if method == "copy":
+            """Copy a file from source to destination. The destination is another zenoh path that must be provided in the parameters as "dest". The source is the key expression of the query.
+            We do this server side to avoid having to read the file into memory on the client side before writing it back to zenoh.
+            """
+            dest_path = query.parameters.get("dest", None)
+            if dest_path is None:
+                logger.error("No destination path provided for copy operation")
+                query.reply(b"Error: No destination path provided for copy operation")
+                return
+            with self.exposed_fs.open(file_path, "rb") as f:
+                f.seek(start)
+                for _ in range(0, end-start, chunk_size):
+                    chunk = f.read(chunk_size)
                     if not chunk:
                         break
-                    entry_hash.update(chunk)
+                    
+                    self.session.get(dest_path, payload=chunk, parameters={"method": "write", "start": start, "end": end})
+            return
 
-            root_hash.update(entry_hash.digest())
+        if method == "read":
 
-        # Final digest
-        query.reply(f"{hash_algo}:{root_hash.hexdigest()}")
+            #ToDo: must set a max chunk size to avoid memory issues.
+            logger.info(f"Reading file at {file_path} with parameters: start={start}, end={end}, chunk_size={chunk_size}")
+            
+            with self.exposed_fs.open(file_path, "rb") as f:
+                f.seek(start)
+                for _ in range(0, end-start, chunk_size):
+                    chunk = f.read(chunk_size)
+                    if not chunk:
+                        break
+                    query.reply(chunk)
 
+    def handle_write(self, query):
+        method = query.parameters.get("method", "write")
+        file_path = self.normalize_path(query.key_expr)
+        start = query.parameters.get("start", 0)
+        end = query.parameters.get("end", None)
 
-    def get_all_handlers(self) -> Generator[Tuple[str, Callable]]:
+        if method == "ticket":
+            query.reply(self.generate_ticket(file_path, "write").encode())
+            return
+
+        if method == "delete":
+            logger.info(f"Deleting file at {file_path}")
+            self.exposed_fs.rm(file_path)
+            return
+
+        if method == "write":
+            logger.info(f"Writing to file at {file_path} with parameters: start={start}, end={end}")
+            with self.exposed_fs.open(file_path, "r+b") as f:
+                f.seek(start)
+                for _ in range(0, end-start, 1024*1024):
+                    chunk = query.receive()
+                    if not chunk:
+                        break
+                    f.write(chunk)
+            return
+        
+        if method == "append":
+            logger.info(f"Appending to file at {file_path}")
+            with self.exposed_fs.open(file_path, "ab") as f:
+                while True:
+                    chunk = query.receive()
+                    if not chunk:
+                        break
+                    f.write(chunk)
+            return
+        
+        if method == "mkdir":
+            logger.info(f"Creating directory at {file_path}")
+            self.exposed_fs.mkdir(file_path)
+            return
+        
+        if method == "rmdir":
+            logger.info(f"Removing directory at {file_path}")
+            self.exposed_fs.rmdir(file_path)
+            return
+        
+
+    def handle_tickets(self, query):
+        """Capability based security using tickets. A client must call read or write with "method=ticket" to generate a ticket for a specific file.
+        A ticket consists of a path like "hostname/files/write/some/file.txt?ticket=12345" where 12345 is a random number. The server keeps track of valid tickets and their associated file paths.
+        The client can then make calls on "hostname/files/ticket/write/some/file.txt?ticket=12345" to write to the file, and the server checks if the ticket is valid for that file before allowing the operation.
+
+        This lets us leverage the existing ACL model of zenoh for read and write paths, file still allowing capability based security for higher performance reads and writes in a distributed system
+        with multiple nodes.
+
+        Internally the server maintains a set of valid tickets. Since the ticket filename is part of the ticket we don't need to store the file path associated with the ticket, we just need to check if the ticket is valid.
+
+        The ticket does not handle fine grained permissions, just access to high level endpoints like read or write. If you want to give
+        someone read but not hash, well you can't.
         """
-        Get all methods that start with "handler_" and return their name (without the prefix) and the method itself as a generator.
-        """
-        for method_name in dir(self):
-            if method_name.startswith("handler_"):
-                operation = method_name.removeprefix("handler_")
-                handler = getattr(self, method_name)
-                yield operation, handler
+        ticket = query.parameters.get("ticket", None)
+        with self.ticket_lock:
+            if not ticket in self.tickets:
+                logger.warning(f"Invalid ticket {ticket} for query {query.key_expr}")
+                query.reply_err(b"Error: Invalid ticket")
+                return
+            # Update last used time for ticket for garbage collection purposes
+            self.tickets[ticket] = time.time()
 
-    def normalize_path(self, path: str) -> str:
-        """Normalize the path by removing the zenoh path prefix and any operation prefixes
+        real_path = query.key_expr.removeprefix(self.zenoh_path + "/ticket/")
+        action = real_path.split("/")[0]
+        if action == "read":
+            self.handle_read(query)
+        elif action == "write":
+            self.handle_write(query)
+        else:
+            logger.warning(f"Invalid action {action} in ticket query {query.key_expr}")
+            query.reply_err(b"Error: Invalid action in ticket query")
+        return
+    
+    def cleanup_tickets(self):
         """
-        path = urllib.parse(path).path
-        path = path.removeprefix(self.zenoh_path)
-        for prefix in (i[0] for i in self.get_all_handlers()):
-            if path.startswith(prefix):
-                path = path.removeprefix(prefix)
-                break #We must break after the first match to avoid removing multiple prefixes
-        return path                
+        Checks the heap for expired tickets.
+        Returns: seconds to sleep until the next expiration check.
+        """
+        now = time.time()
+        next_sleep = self.ticket_ttl  # Default to ticket TTL if no tickets are in the heap.
+
+        with self.ticket_lock:
+            # Process all heap entries whose check_time has passed
+            while self.ticket_heap and self.ticket_heap[0][0] <= now:
+                check_time, ticket = heapq.heappop(self.ticket_heap)
+
+                # Case 1: Ticket manually revoked or somehow missing
+                if ticket not in self.tickets:
+                    continue
+
+                # Case 2: Check actual age relative to TTL
+                actual_age = now - self.tickets[ticket]
+
+                if actual_age >= self.ticket_ttl:
+                    # EXPIRED: Remove the ticket
+                    del self.tickets[ticket]
+                    logger.debug(f"Garbage collected expired ticket: {ticket}")
+                else:
+                    # STILL ACTIVE: Ticket was accessed again after this check was scheduled.
+                    # Reschedule it for its exact new expiration time.
+                    new_expires = self.tickets[ticket] + self.ticket_ttl
+                    heapq.heappush(self.ticket_heap, (new_expires, ticket))
+
+            # If heap isn't empty, sleep until the next item is due
+            if self.ticket_heap:
+                next_sleep = self.ticket_heap[0][0] - now
+
+        # Ensure we don't sleep negative time (logic error safety)
+        return max(0, next_sleep)
+
 
     def expose(self, path: str, zenoh_path: str = f"{hostname}/files/"):
-        self.exposed_fs = UPath(path).absolute()
+        self.tickets = {} # Set of valid tickets for capability based security, key is the full path of the ticket including the token. Value is when the ticket was
+        # last used, for garbage collection purposes. ToDo: implement garbage collection of old tickets.
+        self.ticket_ttl = int(os.getenv("TICKET_TTL", 10 * 60))
+        self.ticket_heap = []
+        self.ticket_lock = threading.Lock()
+
         self.zenoh_path = zenoh_path.removeprefix("/").removesuffix("/")
+        self.exposed_fs = UPath(path).absolute()
 
         logger.info(f"Exposing {self.exposed_fs} at {self.zenoh_path}")
-        #assert exposed_fs.exists(), f"Path {exposed_fs} does not exist"
 
+        self.session = zenoh.open(zenoh.Config())
+        self.session.declare_queryable(self.zenoh_path + "read/**", self.handle_read)
+        self.session.declare_queryable(self.zenoh_path + "write/**", self.handle_write)
+        self.session.declare_queryable(self.zenoh_path + "ticket/**", self.handle_tickets)
 
-        with zenoh.open(zenoh.Config()) as session:
-            read_path = self.zenoh_path+"/read/**"
-            for handler_name, handler in self.get_all_handlers():
-                handler_name = self.zenoh_path + "/" + handler_name + "/**"
-                session.declare_queryable(handler_name, handler)
-                logger.debug(f"Declaring handler for {handler_name} at {handler}")
+        try:
+            while True:
 
-            try:
-                while True:
-                    time.sleep(1)
-            except KeyboardInterrupt:
-                logger.info("Shutting down")
+                sleep_time = self.cleanup_tickets()
+                time.sleep(sleep_time)
+
+        except KeyboardInterrupt:
+            logger.info("Shutting down")
 
 
 class ZenohFS(AbstractFileSystem):
